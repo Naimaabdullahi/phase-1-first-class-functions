a2ba21ce472294ab8ef8a679fe8e318de3b97fab
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-03-08 14:46:39 +0300

    anonymous function

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function named() {
+        console.log ('before all')
+    } 
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        console.log ('before all')
+    }
+}
+
